c63e5a29e481d6e7cd2c6d3a346ae0f983d99d7a
Robson Taye <ragnarrobie@gmail.com>
2026-08-03 10:51:48 +0300

    hooray

--- Chef-Claude/src/App.jsx
+++ Chef-Claude/src/App.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Body from "./Components/Body/Body";
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
-      <Body />
       <Footer />
     </div>
   );
--- Chef-Claude/src/Components/Header/Header.jsx
+++ Chef-Claude/src/Components/Header/Header.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
+  const ingredients = ["Chicken", "Tomatoes", "Oregano"];
+    function submit(event){
+        event.preventDefault()
+        const newList = event.target.ingredient.value
+         ingredients.push(newList)
+         console.log("pushed")
+    }
+const list = ingredients.map((ingredient)=>{
+return <li key = {ingredient}>{ingredient}</li>
+})
+
   return (
-    <div className="flex items-center gap-3 my-12">
-  <form>
-    <input
-      type="text"
-      placeholder="Search..."
-      className="w-200 h-12 px-2 ml-64 border border-gray-300 rounded-lg"
-    />
-  </form>
+    <div className="my-12">
+      {/* Form */}
+      <form
+        onSubmit={submit}
+        className="flex items-center justify-center gap-3"
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-96 h-12 px-4 border border-gray-300 rounded-lg"
+          name="indregient"
+        />
 
-  <button
-    className="h-12 w-54 px-6 flex items-center justify-center
-               bg-black text-white font-semibold rounded-lg
-               hover:bg-gray-500 cursor-pointer"
-  >
-    Search
-  </button>
-</div>
-  )
-}
+        <button
+          type="submit"
+          className="h-12 w-32 flex items-center justify-center
+                     bg-black text-white font-semibold rounded-lg
+                     hover:bg-gray-500 cursor-pointer"
+        >
+          Search
+        </button>
+      </form>
 
-export default Header
+      {/* List */}
+      <ul className="list-disc w-96 mx-auto mt-8 pl-6">{list}</ul>
+    </div>
+  );
+};
+
+export default Header;
