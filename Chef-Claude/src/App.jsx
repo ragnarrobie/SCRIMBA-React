39941257ed82680f643a7625aaa2630103daf565
Robson Taye <ragnarrobie@gmail.com>
2026-08-05 10:19:20 +0300

    deletd footer

--- Chef-Claude/src/App.jsx
+++ Chef-Claude/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Header />
-      <Footer />
     </div>
   );
 };
