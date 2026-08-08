be606a9a5cd980049d983198b48953e6ffac4999
Robson Taye <ragnarrobie@gmail.com>
2026-08-08 11:57:00 +0300

    Trained useEffect

--- Meme-Generator/Meme-Generator/src/App.jsx
+++ Meme-Generator/Meme-Generator/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Body from "./Components/Body/Body";
+import Navbar from "./Components/Navbar/Navbar";
 const App = () => {
   return (
     <div>
-      <Header />
-      <Body />
+      <Navbar />
     </div>
   );
 };
--- Meme-Generator/Meme-Generator/src/Components/Navbar/Navbar.jsx
+++ Meme-Generator/Meme-Generator/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [id,setId] = useState(1)
+  const [starWarsData, setStarWarsData] = useState(null);
+  const [count, setCount] = useState(1);
+
+   useEffect(() => {
+    fetch(`https://swapi.tech/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data.result.properties))
+      .catch((err) => console.error("fetch failed", err));
+  }, [count]); 
+  
   return (
     <div>
-      
-    </div>
-  )
-}
+      <h2>{count}</h2>
 
-export default Navbar
+      <button
+        onClick={() => {
+          setCount(prevcount =>prevcount + 1);
+        }}
+      >
+        Get next character
+      </button>
+       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Navbar;
